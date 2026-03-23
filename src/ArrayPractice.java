3aaf2855f06a8acb887efa6ff15e2fd0ce687ec6
onjamm <75151498+onjamm@users.noreply.github.com>
2026-03-22 19:18:22 -0700

    Created array, set the values (individually), got the value fo the array at index 2, and go thte length of the array, with int length

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,13 +1,22 @@
 public class ArrayPractice {
   public static void main(String[] args) {
     // Create an array of Strings of size 4
-
+    String[] words = new String[4];
     // Set the value of the array at each index to be a different String
+    words[0] = "Sam";
+    words[1] = "Dean";
+    words[2] = "Castiel";
+    words[3] = "Crowley";
+    
     // It's OK to do this one-by-one
 
     // Get the value of the array at index 2
+    String indexTwo = words[2];
+    System.out.println(indexTwo);
 
     // Get the length of the array
+    int length = words.length;
+    System.out.println(length);
 
     // Iterate over the array using a traditional for loop and print out each item
 
