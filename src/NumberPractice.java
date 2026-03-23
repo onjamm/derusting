b93801dc7b64fd0128fe88f7e6485a4c24b25c79
onjamm <75151498+onjamm@users.noreply.github.com>
2026-03-22 19:55:14 -0700

    added the integer division, as well as made the output prettier with some nice sout messages

--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -2,11 +2,14 @@ public class NumberPractice {
   public static void main(String args[]) {
     // Create a float with a negative value and assign it to a variable
     float negative = -3.1403403403213f;
+    System.out.println("Negative float: " +negative);
 
     // Create an int with a positive value and assign it to a variable
     int positive = 999;
+    System.out.println("Positive: " +positive);
     // Use the modulo % operator to find the remainder when the int is divided by 3
     int remainder = positive % 3;
+    System.out.println("Remainder: " + remainder);
     // Use the modulo % operator to determine whether the number is even
     int even = positive % 2;
     // (A number is even if it has a remainder of zero when divided by 2)
@@ -20,6 +23,8 @@ public class NumberPractice {
     // if the number is odd.
 
     // Divide the number by another number using integer division
+    int division = positive / 3 ;
+    System.out.println("Integer division: " + division);
 
     /*
      * Reminder!
